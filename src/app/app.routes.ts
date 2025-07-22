ca2b2790a222238de48950e269a0564c65eb8618
Raygust <harol5477@gmail.com>
2025-07-22 18:59:44 -0400

    feat: importations and routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPpageComponent } from './views/login-ppage/login-ppage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPpageComponent,
+  },{ path: '',   redirectTo: '/login', pathMatch: 'full' }
+];
