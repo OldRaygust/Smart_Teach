5a30040c34c0fea4df56f33345726d76cc9d829b
Raygust <harol5477@gmail.com>
2025-09-29 17:14:02 -0300

    feat: asignaturas vista

--- src/app/components/asignatura/asignatura.component.ts
+++ src/app/components/asignatura/asignatura.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Asignatura {
+  nombre: string;
+  curso: string;
+  descripcion: string;
+  imagen?: string;
+}
 
 @Component({
   selector: 'app-asignatura',
   imports: [],
   templateUrl: './asignatura.component.html',
-  styleUrl: './asignatura.component.scss'
+  styleUrl: './asignatura.component.scss',
 })
-export class AsignaturaComponent {
+export class AsignaturaComponent implements OnInit {
+  asignaturas: Asignatura[] = [];
 
+  ngOnInit(): void {
+    this.asignaturas = [
+      //Aqui se podra hacer que sea dinamico.
+      {
+        nombre: 'Matemáticas',
+        curso: '1° Medio',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        imagen: '',
+      },
+      {
+        nombre: 'Historia',
+        curso: '2° Medio',
+        descripcion: 'Suspendisse id lorem in mi fermentum porta.',
+        imagen: '',
+      },
+      {
+        nombre: 'Lenguaje',
+        curso: '3° Medio',
+        descripcion: 'Nulla facilisi. Donec vel nisl a justo.',
+        imagen: '',
+      },
+    ];
+  }
+  definirPlanificacion(asignatura: Asignatura) {
+    console.log('Definir planificación para:', asignatura);
+    // Aquí puedes navegar a otra ruta o abrir un modal
+  }
 }
