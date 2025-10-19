52baff39918aa0fd1a2753955c685e5a201d92ab
Raygust <harol5477@gmail.com>
2025-10-19 19:50:28 -0300

    feat: creado formulario para editar planificacion

--- src/app/components/formulario-edicion-planificacion/formulario-edicion-planificacion.component.ts
+++ src/app/components/formulario-edicion-planificacion/formulario-edicion-planificacion.component.ts
@@ -1,11 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+interface Experience {
+  titulo: string;
+  empleador: string;
+  ciudad: string;
+  inicio: string;
+  fin: string;
+  descripcion: string;
+}
 
 @Component({
   selector: 'app-formulario-edicion-planificacion',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './formulario-edicion-planificacion.component.html',
-  styleUrl: './formulario-edicion-planificacion.component.scss'
+  styleUrl: './formulario-edicion-planificacion.component.scss',
 })
 export class FormularioEdicionPlanificacionComponent {
+  experiences: Experience[] = [];
 
+  newExperience: Experience = {
+    titulo: '',
+    empleador: '',
+    ciudad: '',
+    inicio: '',
+    fin: '',
+    descripcion: '',
+  };
+
+  addExperience() {
+    this.experiences.push({ ...this.newExperience });
+    this.newExperience = {
+      titulo: '',
+      empleador: '',
+      ciudad: '',
+      inicio: '',
+      fin: '',
+      descripcion: '',
+    };
+  }
 }
--- src/app/views/formulario/formulario.component.ts
+++ src/app/views/formulario/formulario.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../components/menu/menu.component';
+import { FormularioEdicionPlanificacionComponent } from '../../components/formulario-edicion-planificacion/formulario-edicion-planificacion.component';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [MenuComponent, FormularioEdicionPlanificacionComponent],
   templateUrl: './formulario.component.html',
-  styleUrl: './formulario.component.scss'
+  styleUrl: './formulario.component.scss',
 })
-export class FormularioComponent {
-
-}
+export class FormularioComponent {}
