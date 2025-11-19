1730851f2674f7dc60b5b316a916e3b84d0c7a25
Harol Chavez <harol5477@gmail.com>
2025-11-19 17:34:16 -0300

    feat: pantalla cargada

--- src/app/views/avance-asignatura/avance-asignatura.component.ts
+++ src/app/views/avance-asignatura/avance-asignatura.component.ts
@@ -1,8 +1,10 @@
+import { MenuComponent } from './../../components/menu/menu.component';
 import { Component } from '@angular/core';
+import { AvanceComponent } from "../../components/avance/avance.component";
 
 @Component({
   selector: 'app-avance-asignatura',
-  imports: [],
+  imports: [MenuComponent, AvanceComponent],
   templateUrl: './avance-asignatura.component.html',
   styleUrl: './avance-asignatura.component.scss'
 })
