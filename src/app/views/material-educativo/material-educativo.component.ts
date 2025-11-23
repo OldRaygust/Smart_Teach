51fcf7f09c65bd73efde90a34fd22ded9867a651
Raygust <harol5477@gmail.com>
2025-11-23 17:19:57 -0300

    feat: enrutamiento del menu y creacion del repositorio

---
+++ src/app/views/material-educativo/material-educativo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-material-educativo',
+  imports: [],
+  templateUrl: './material-educativo.component.html',
+  styleUrl: './material-educativo.component.scss'
+})
+export class MaterialEducativoComponent {
+
+}
