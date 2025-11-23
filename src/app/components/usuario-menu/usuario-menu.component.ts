8d7697a2911ab4e6ccf6f76160a28589290d74c6
Raygust <harol5477@gmail.com>
2025-11-23 08:57:30 -0300

    feat: capsula usuario

---
+++ src/app/components/usuario-menu/usuario-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-menu',
+  imports: [],
+  templateUrl: './usuario-menu.component.html',
+  styleUrl: './usuario-menu.component.scss'
+})
+export class UsuarioMenuComponent {
+
+}
